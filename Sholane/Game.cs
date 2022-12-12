4e0e02d4af87b8a132805e021fdb0e85927df2a8
Second <artemtrushnikov@mail.ru>
2022-12-13 00:07:46 +1000

    Добавьте файлы проекта.

---
+++ Sholane/Game.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Sholane
+{
+    internal class Game : GameWindow
+    {
+        public Game(GameWindowSettings gSettings, NativeWindowSettings nSettings) : base(gSettings, nSettings)
+        {
+            VSync = VSyncMode.Off;
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+        }
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            SwapBuffers();
+        }
+    }
+}
---
+++ Sholane/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Sholane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindowSettings gSettings = GameWindowSettings.Default;
+            NativeWindowSettings nSettings = new NativeWindowSettings()
+            {
+                Title = "Sholane",
+                Size = (500, 500),
+                Flags = ContextFlags.Default,
+                Profile = ContextProfile.Compatability
+            };
+            Game game = new Game(gSettings, nSettings);
+            game.Run();
+        }
+    }
+}
