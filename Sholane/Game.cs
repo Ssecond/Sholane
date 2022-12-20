1675e05a125d1643fd39c093c172755986883843
Second <artemtrushnikov@mail.ru>
2022-12-21 01:07:40 +1000

    Added a plane

--- Sholane/Game.cs
+++ Sholane/Game.cs
@@ -4,20 +4,22 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using Sholane.TextureProcess;
-using System.Drawing;
 
 namespace Sholane
 {
     internal class Game : GameWindow
     {
-        Matrix4 ortho;
-        Entity entity, background;
+        private Matrix4 ortho;
+        private Entity rocket, plane, background;
+        private double lag = 0, TIME_PER_FRAME = 0.001;
+        private Keys lastKeyboardState;
         internal Game(GameWindowSettings gSettings, NativeWindowSettings nSettings) : base(gSettings, nSettings)
         {
             VSync = VSyncMode.On;
             GL.Enable(EnableCap.Texture2D);
 
-            entity = new Entity(33, 60, "Content\\Rocket.png", Vector2.Zero, BufferUsageHint.DynamicDraw);
+            rocket = new Entity(33, 60, "Content\\Rocket.png", new Vector2(nSettings.Size.Y / 2 - 15, nSettings.Size.X - 60), BufferUsageHint.DynamicDraw);
+            plane = new Entity(60, 28, "Content\\Plane.png", Vector2.Zero, BufferUsageHint.DynamicDraw);
             background = new Entity(nSettings.Size.X, nSettings.Size.Y, "Content\\Sky.jpg", Vector2.Zero);
         }
 
@@ -32,7 +34,6 @@ namespace Sholane
         protected override void OnResize(ResizeEventArgs e)
         {
             base.OnResize(e);
-
             GL.Viewport(0, 0, this.Size.X, this.Size.Y);
 
             ortho = Matrix4.CreateOrthographicOffCenter(0, this.Size.X, this.Size.Y, 0, -1, 1);
@@ -41,21 +42,17 @@ namespace Sholane
             GL.LoadMatrix(ref ortho);
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadIdentity();
-
             background.Resize(this.Size.X, this.Size.Y);
         }
-        double lag = 0, TIME_PER_FRAME = 0.001;
-        Keys lastKeyboardState;
         protected override void OnUpdateFrame(FrameEventArgs args)
         {
             base.OnUpdateFrame(args);
-
             lag += args.Time;
             if (lag > TIME_PER_FRAME)
             {
                 while (lag > TIME_PER_FRAME)
                 {
-                    KeyPressedMoveTheEntity(lastKeyboardState);
+                    MoveEntities(lastKeyboardState);
                     lag -= TIME_PER_FRAME;
                 }
             }
@@ -64,12 +61,11 @@ namespace Sholane
         {
             base.OnRenderFrame(args);
             GL.Clear(ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
-
             background.Draw();
-            entity.Draw();
+            rocket.Draw();
+            plane.Draw();
             GL.LoadIdentity();
             GL.End();
-
             SwapBuffers();
         }
         protected override void OnKeyDown(KeyboardKeyEventArgs e)
@@ -77,34 +73,35 @@ namespace Sholane
             base.OnKeyDown(e);
             lastKeyboardState = e.Key;
         }
-        private void KeyPressedMoveTheEntity(Keys key)
+        private void MoveEntities(Keys key)
         {
             switch (key)
             {
                 case Keys.Up:
                 case Keys.W:
-                    entity.Move(new Vector2(-0.0f, -0.1f));
+                    rocket.Move(new Vector2(-0.0f, -0.1f));
                     break;
 
                 case Keys.Left:
                 case Keys.A:
-                    entity.Move(new Vector2(-0.1f, 0.0f));
+                    rocket.Move(new Vector2(-0.1f, 0.0f));
                     break;
 
                 case Keys.Down:
                 case Keys.S:
-                    entity.Move(new Vector2(0.0f, 0.1f));
+                    rocket.Move(new Vector2(0.0f, 0.1f));
                     break;
 
                 case Keys.Right:
                 case Keys.D:
-                    entity.Move(new Vector2(0.1f, 0.0f));
+                    rocket.Move(new Vector2(0.1f, 0.0f));
                     break;
 
                 case Keys.Escape:
                     Close();
                     break;
             }
+            plane.Move(new Vector2(0.01f, 0.0f));
         }
     }
 }
